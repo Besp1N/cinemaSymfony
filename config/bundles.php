dd1286df618b0e4afe025fb91bfcd76c8f5e2e11
Besp1N <kackar87@wp.pl>
2024-01-09 17:04:37 +0000

    fixed bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,12 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-=======
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
->>>>>>> c95edcb8679bd5c2c20fbdf29f85e43234b44177
 ];
