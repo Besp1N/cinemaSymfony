4120dceb211f56fd88f8c072961c641cba4a97e4
PendolinoVoyager <q6894kjz8x34@gmail.com>
2024-01-09 17:33:02 +0100

    controller added

---
+++ src/Controller/DupaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DupaController extends AbstractController
+{
+    #[Route('/dupa', name: 'app_dupa')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/DupaController.php',
+            'kacper' => 'to bambik'
+        ]);
+    }
+}
