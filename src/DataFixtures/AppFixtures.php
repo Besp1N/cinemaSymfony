5c32280d9aa3985ca5cabcc4313e48858cc13a44
Besp1N <kackar87@wp.pl>
2024-01-10 17:03:05 +0000

    added hints in data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,30 +13,33 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //creating a cinema
         $cinema = new Cinema();
         $cinema->setName("multikino");
         $cinema->setCity("slupsk");
         $cinema->setAddress("zielona4");
 
+        // creating 2 movie theaters
         $movieTheater1 = new MovieTheaters();
         $movieTheater1->setName("sala1");
         $movieTheater2 = new MovieTheaters();
         $movieTheater2->setName("sala2");
 
+        // creating 2 seats
         $seat1 = new Seat();
         $seat1->setSeatType("vip");
         $seat1->setSeatRow("A");
         $seat1->setSeatNumber("7");
-
         $seat2 = new Seat();
         $seat2->setSeatType("vip");
         $seat2->setSeatRow("B");
         $seat2->setSeatNumber("8");
 
-
+        // adding 2 seats to movie theaters
         $movieTheater1->addSeat($seat1);
         $movieTheater2->addSeat($seat2);
 
+        // adding movie theaters to cinema and push to database
         $cinema->addMovieTheater($movieTheater1);
         $cinema->addMovieTheater($movieTheater2);
         $manager->persist($cinema);
