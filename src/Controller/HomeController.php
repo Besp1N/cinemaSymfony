0ad711d2d54a67ab0e0eb4b5bcdf9bec37512e1d
Besp1N <kackar87@wp.pl>
2024-01-11 18:01:37 +0000

    added movie form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Movie;
+use App\Form\MovieType;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +27,28 @@ class HomeController extends AbstractController
     #[Route('/{movie}', name: 'app_home_movie')]
     public function showMovie(Movie $movie): Response
     {
-        dd($movie);
+        return $this->render('home/movie.html.twig', [
+            "movie" => $movie
+        ]);
     }
+
+    #[Route('/add_movie', name: 'app_home_movie_add', priority: 2)]
+    public function addMovie(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MovieType::class, new Movie());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $movie = $form->getData();
+            $entityManager->persist($movie);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('home/add.html.twig', [
+            "form" => $form
+        ]);
+    }
+
 }
