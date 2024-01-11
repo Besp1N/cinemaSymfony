e8dd2d98bedf437d697ff70fee0f940bdfe19f12
Besp1N <kackar87@wp.pl>
2024-01-11 19:41:15 +0000

    added movie crud controller

---
+++ src/Controller/Admin/MovieCrudController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Movie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+
+class MovieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Movie::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+//            DateTimeField::new('duration')->renderAsText(),
+            TextField::new('description'),
+            TextField::new('genre'),
+//            DateTimeField::new('relase_year'),
+            TextField::new('director'),
+            NumberField::new('rating'),
+            TextField::new('image')
+        ];
+    }
+}
