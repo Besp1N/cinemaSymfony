0241ad15e95c5a20cd0b64b59242913438956d0e
Besp1N <kackar87@wp.pl>
2024-01-12 10:21:47 +0000

    added last user controller and retuned cinemas to admin view

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,18 +8,34 @@ use App\Entity\MovieTheater;
 use App\Entity\Reservation;
 use App\Entity\Screening;
 use App\Entity\Seat;
+use App\Entity\User;
+use App\Repository\CinemaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render("admin/index.html.twig");
+        $cinemaRepository = $this->entityManager->getRepository(Cinema::class);
+        $cinemas = $cinemaRepository->findAll();
+
+        return $this->render("admin/index.html.twig", [
+            "cinemas" => $cinemas
+        ]);
     }
 
     public function configureDashboard(): Dashboard
@@ -31,11 +47,15 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToRoute('back to home', 'fas fa-arrow-left', 'app_home');
         yield MenuItem::linkToCrud('Movies', 'fas fa-list', Movie::class);
         yield MenuItem::linkToCrud('Movie Theaters', 'fas fa-list', MovieTheater::class);
         yield MenuItem::linkToCrud('Cinemas', "fas fa-list", Cinema::class);
         yield MenuItem::linkToCrud('Seats', 'fas fa-list', Seat::class);
         yield MenuItem::linkToCrud('Screening', "fas fa-list", Screening::class);
         yield MenuItem::linkToCrud('Reservation', "fas fa-list", Reservation::class);
+        yield MenuItem::linkToCrud('users', 'fas fa-list', User::class);
     }
+
+
 }
