6787afb97522558b0f4793ef0440838505d38b02
Besp1N <kackar87@wp.pl>
2024-01-20 20:19:56 +0000

    added ocupied seats logic

--- src/Controller/Reservation/ReservationController.php
+++ src/Controller/Reservation/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Reservation;
 
 use App\Entity\Reservation;
 use App\Entity\Screening;
+use App\Repository\ReservationRepository;
 use App\Repository\SeatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use \Symfony\Component\HttpFoundation\Request;
@@ -14,15 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReservationController extends AbstractController
 {
     #[Route('/reservation/{screening}', name: 'app_reservation', priority: 7)]
-    public function index(Request $request , Screening $screening, SeatRepository $seatRepository, EntityManagerInterface $entityManager): Response
+    public function index(Request $request , Screening $screening, SeatRepository $seatRepository, ReservationRepository $reservationRepository, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
         $movieTheaterId = $screening->getMovieTheater()->getId();
         $seats = $seatRepository->findByMovieTheater($movieTheaterId);
 
+        $reservations = $reservationRepository->findBy(['screening' => $screening]);
+
+        // iteruje se po rezerwacjach gdzie indexy sa id siedzien i ustawiam je na true
+        $occupiedSeats = [];
+        foreach ($reservations as $reservation) {
+            $occupiedSeats[$reservation->getSeat()->getId()] = true;
+        }
+
+
         // bardzo dobra radziecka metoda, a co lepsze to nawet dziala
         // a bez jaj to musze zrobic ReservationType form i z niej to brac ale juz na dzisiaj tak bedzie
-
         if ($request->isMethod('POST')) {
             $selectedSeatId = $request->request->get('selectedSeat');
             $selectedSeat = $seatRepository->find($selectedSeatId);
@@ -34,7 +43,6 @@ class ReservationController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'zrobiles rezerwacje!');
-
             return $this->redirectToRoute('app_user', [
                 'user' => $user->getId()
             ]);
