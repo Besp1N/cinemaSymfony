b68bbd2316f92876bbd2c5017d63add80ad09a59
Besp1N <kackar87@wp.pl>
2024-01-21 22:49:46 +0000

    added fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -35,8 +35,6 @@ class AppFixtures extends Fixture
         $user1->setRoles(['ROLE_USER']);
         $user1->setPhoneNumber('+48 123 456 789');
         $user1->setPassword($this->userPasswordHasher->hashPassword($user1, '12345678'));
-        $manager->persist($user1);
-        $manager->flush();
 
         $user1Admin = new User();
         $user1Admin->setName('Kacper');
@@ -48,8 +46,6 @@ class AppFixtures extends Fixture
         $user1Admin->setRoles(['ROLE_ADMIN']);
         $user1Admin->setPhoneNumber('+48 123 456 789');
         $user1Admin->setPassword($this->userPasswordHasher->hashPassword($user1Admin, '12345678'));
-        $manager->persist($user1Admin);
-        $manager->flush();
 
         $user2 = new User();
         $user2->setName('Klaudiusz');
@@ -61,8 +57,6 @@ class AppFixtures extends Fixture
         $user2->setRoles(['ROLE_USER']);
         $user2->setPhoneNumber('+48 987 654 321');
         $user2->setPassword($this->userPasswordHasher->hashPassword($user2, '12345678'));
-        $manager->persist($user2);
-        $manager->flush();
 
         $user2Admin = new User();
         $user2Admin->setName('Klaudiusz');
@@ -74,8 +68,6 @@ class AppFixtures extends Fixture
         $user2Admin->setRoles(['ROLE_ADMIN']);
         $user2Admin->setPhoneNumber('+48 987 654 321');
         $user2Admin->setPassword($this->userPasswordHasher->hashPassword($user2Admin, '12345678'));
-        $manager->persist($user1Admin);
-        $manager->flush();
 
         $user3 = new User();
         $user3->setName('Anna');
@@ -87,8 +79,6 @@ class AppFixtures extends Fixture
         $user3->setRoles(['ROLE_USER']);
         $user3->setPhoneNumber('+48 601 234 567');
         $user3->setPassword($this->userPasswordHasher->hashPassword($user3, '12345678'));
-        $manager->persist($user3);
-        $manager->flush();
 
         $user4 = new User();
         $user4->setName('Anastazja');
@@ -100,8 +90,6 @@ class AppFixtures extends Fixture
         $user4->setRoles(['ROLE_USER']);
         $user4->setPhoneNumber('+48 602 345 678');
         $user4->setPassword($this->userPasswordHasher->hashPassword($user4, '12345678'));
-        $manager->persist($user4);
-        $manager->flush();
 
         $user5 = new User();
         $user5->setName('Sylwia');
@@ -113,8 +101,6 @@ class AppFixtures extends Fixture
         $user5->setRoles(['ROLE_USER']);
         $user5->setPhoneNumber('+48 603 456 789');
         $user5->setPassword($this->userPasswordHasher->hashPassword($user5, '12345678'));
-        $manager->persist($user5);
-        $manager->flush();
 
         $user6 = new User();
         $user6->setName('Adam');
@@ -126,8 +112,6 @@ class AppFixtures extends Fixture
         $user6->setRoles(['ROLE_USER']);
         $user6->setPhoneNumber('+48 604 567 890');
         $user6->setPassword($this->userPasswordHasher->hashPassword($user6, '12345678'));
-        $manager->persist($user6);
-        $manager->flush();
 
         $user7 = new User();
         $user7->setName('Kordian');
@@ -139,8 +123,6 @@ class AppFixtures extends Fixture
         $user7->setRoles(['ROLE_USER']);
         $user7->setPhoneNumber('+48 605 678 901');
         $user7->setPassword($this->userPasswordHasher->hashPassword($user7, '12345678'));
-        $manager->persist($user7);
-        $manager->flush();
 
         $user8 = new User();
         $user8->setName('Karolina');
@@ -152,12 +134,14 @@ class AppFixtures extends Fixture
         $user8->setRoles(['ROLE_USER']);
         $user8->setPhoneNumber('+48 606 789 012');
         $user8->setPassword($this->userPasswordHasher->hashPassword($user8, '12345678'));
-        $manager->persist($user8);
-        $manager->flush();
 
 
         // tworzenie 8 filmow po 2 gatunki
         // Gatunek Adventure - Film 1
+        // kazdy film trwa 2 godziny
+        $duration = new DateTimeImmutable();
+        $duration->setTime(2,0);
+
         $movie1 = new Movie();
         $movie1->setTitle('Indiana Jones and Raiders of the lost Ark');
         $movie1->setDescription(
@@ -165,12 +149,10 @@ class AppFixtures extends Fixture
              of the fabled Lost Ark of the Covenant, racing a bitter rival and his Nazi cohorts to the prize.');
         $movie1->setDirector('Steven Spielberg');
         $movie1->setGenre('Adventure');
-        $movie1->setDuration(new DateTimeImmutable());
+        $movie1->setDuration($duration);
         $movie1->setRating(5.0);
         $movie1->setReleaseYear(new DateTime());
         $movie1->setImage("images/poster1.jpg");
-        $manager->persist($movie1);
-        $manager->flush();
 
         // Gatunek Adventure - Film 2
         $movie2 = new Movie();
@@ -181,12 +163,10 @@ class AppFixtures extends Fixture
          Hammond and his team of dedicated scientists have populated the park with genetically engineered dinosaurs.');
         $movie2->setDirector('Steven Spielberg');
         $movie2->setGenre('Adventure');
-        $movie2->setDuration(new DateTimeImmutable());
+        $movie1->setDuration($duration);
         $movie2->setRating(4.0);
         $movie2->setReleaseYear(new DateTime());
         $movie2->setImage("images/poster2.jpg");
-        $manager->persist($movie2);
-        $manager->flush();
 
         // Gatunek Crime - Film 3
         $movie3 = new Movie();
@@ -198,12 +178,10 @@ class AppFixtures extends Fixture
          on the wedding day of his daughter, Connie (Talia Shire).');
         $movie3->setDirector('Francis Ford Coppola');
         $movie3->setGenre('Crime');
-        $movie3->setDuration(new DateTimeImmutable());
+        $movie3->setDuration($duration);
         $movie3->setRating(5.0);
         $movie3->setReleaseYear(new DateTime());
         $movie3->setImage("images/poster3.jpg");
-        $manager->persist($movie3);
-        $manager->flush();
 
         // Gatunek 2 - Film 4
         $movie4 = new Movie();
@@ -213,12 +191,10 @@ class AppFixtures extends Fixture
          and a pair of diner bandits intertwine in four tales of violence and redemption.');
         $movie4->setDirector('Quentin Tarantino');
         $movie4->setGenre('Crime');
-        $movie4->setDuration(new DateTimeImmutable());
+        $movie4->setDuration($duration);
         $movie4->setRating(3.0);
         $movie4->setReleaseYear(new DateTime());
         $movie4->setImage("images/poster4.jpg");
-        $manager->persist($movie4);
-        $manager->flush();
 
         // Gatunek Science Fiction - Film 5
         $movie5 = new Movie();
@@ -229,12 +205,10 @@ class AppFixtures extends Fixture
          but his tragic past may doom the project and his team to disaster.');
         $movie5->setDirector('Christopher Nolan');
         $movie5->setGenre('Science Fiction');
-        $movie5->setDuration(new DateTimeImmutable());
+        $movie5->setDuration($duration);
         $movie5->setRating(4.5);
         $movie5->setReleaseYear(new DateTime());
         $movie5->setImage("images/poster5.jpg");
-        $manager->persist($movie5);
-        $manager->flush();
 
         // Gatunek Science Fiction - Film 6
         $movie6 = new Movie();
@@ -244,12 +218,10 @@ class AppFixtures extends Fixture
          when artificial human adults called "replicants" come to Earth.');
         $movie6->setDirector('Ridley Scott');
         $movie6->setGenre('Science Fiction');
-        $movie6->setDuration(new DateTimeImmutable());
+        $movie6->setDuration($duration);
         $movie6->setRating(4.0);
         $movie6->setReleaseYear(new DateTime());
         $movie6->setImage("images/poster6.jpg");
-        $manager->persist($movie6);
-        $manager->flush();
 
         // Gatunek Drama - Film 7
         $movie7 = new Movie();
@@ -261,12 +233,10 @@ class AppFixtures extends Fixture
          and six Academy Awards, including best picture.');
         $movie7->setDirector('Robert Zemeckis');
         $movie7->setGenre('Drama');
-        $movie7->setDuration(new DateTimeImmutable());
+        $movie7->setDuration($duration);
         $movie7->setRating(2.0);
         $movie7->setReleaseYear(new DateTime());
         $movie7->setImage("images/poster7.jpg");
-        $manager->persist($movie7);
-        $manager->flush();
 
         // Gatunek Drama - Film 8
         $movie8 = new Movie();
@@ -278,12 +248,11 @@ class AppFixtures extends Fixture
          (Michael Stuhlbarg), an archaeology professor.');
         $movie8->setDirector('Frank Darabont');
         $movie8->setGenre('Drama');
-        $movie8->setDuration(new DateTimeImmutable());
+        $movie8->setDuration($duration);
         $movie8->setRating(5.0);
         $movie8->setReleaseYear(new DateTime());
         $movie8->setImage("images/poster8.jpg");
-        $manager->persist($movie8);
-        $manager->flush();
+
 
 
         // Tworzenie 3 kin
@@ -367,150 +336,53 @@ class AppFixtures extends Fixture
 
 
         // Dodawanie seansow
-        $screening = new Screening();
-        $screening->setMovie($movie1);
-        $startTime = new DateTimeImmutable();
-        $startTime->setTime(12,0);
-        $screening->setStartTime($startTime);
+        $screening1 = new Screening();
+        $screening1->setMovie($movie1);
+        $screening1->setStartTime(new DateTimeImmutable('2024-02-01 12:00'));
+
+        $screening2 = new Screening();
+        $screening2->setMovie($movie2);
+        $screening2->setStartTime(new DateTimeImmutable('2024-02-02 12:00'));
+
+        $screening3 = new Screening();
+        $screening3->setMovie($movie3);
+        $screening3->setStartTime(new DateTimeImmutable('2024-02-03 12:00'));
+
+        $screening4 = new Screening();
+        $screening4->setMovie($movie4);
+        $screening4->setStartTime(new DateTimeImmutable('2024-02-04 12:00'));
+
+        $screening5 = new Screening();
+        $screening5->setMovie($movie5);
+        $screening5->setStartTime(new DateTimeImmutable('2024-02-05 12:00'));
+
+        $screening6 = new Screening();
+        $screening6->setMovie($movie6);
+        $screening6->setStartTime(new DateTimeImmutable('2024-02-06 12:00'));
+
+        $movieTheater1->addScreening($screening1);
+        $movieTheater2->addScreening($screening2);
+        $movieTheater3->addScreening($screening3);
+        $movieTheater4->addScreening($screening4);
+        $movieTheater5->addScreening($screening5);
+        $movieTheater6->addScreening($screening6);
 
 
+        // Blagam zeby to dzialalo <3
+        $manager->persist($user1);
+        $manager->persist($user1Admin);
+        $manager->persist($user2);
+        $manager->persist($user2Admin);
+        $manager->persist($user3);
+        $manager->persist($user4);
+        $manager->persist($user5);
+        $manager->persist($user7);
+        $manager->persist($user8);
 
-
-        // Tworzenie rezerwacji dla użytkownika, miejsca i seansu
-        $reservation = new Reservation();
-        $reservation->setUser($user123);
-        $reservation->setSeat($seat);
-        $reservation->setScreening($screening);
-
-        // Dodawanie elementów do relacji
-        $user123->addReservation($reservation);
-        $movieTheater->addSeat($seat);
-        $movieTheater->addScreening($screening);
-        $cinema->addMovieTheater($movieTheater);
-
-        // Persystencja obiektów do bazy danych
-        $manager->persist($user123);
-        $manager->persist($movie);
-        $manager->persist($cinema);
-
-        // Flush do zapisania zmian w bazie danych
-        $manager->flush();
-
-        $movie = new Movie();;
-        $movie->setTitle('Star Wars');
-        $movie->setDescription('cool movie');
-        $movie->setDirector('George Lucas');
-        $movie->setGenre('Sci-Fi');
-        $movie->setDuration(new DateTimeImmutable());
-        $movie->setReleaseYear(new DateTime());
-        $movie->setImage("images/poster2.jpg");
-        $movie->setRating(5.0);
-
-
-        $adminUser = new User();
-        $adminUser->setName('admin');
-        $adminUser->setLastname('admin');
-        $adminUser->setProfilePicture('dupa/dupa');
-        $adminUser->setPassword($this->userPasswordHasher->hashPassword($adminUser, 'dupa'));
-        $adminUser->setRoles(['ROLE_ADMIN']);
-        $adminUser->setCreatedAt(new DateTimeImmutable());
-        $adminUser->setPhoneNumber('+48 123456789');
-        $adminUser->setEmail('admin@admin.pl');
-        $adminUser->setBio('DUPA');
-
-        $manager->persist($adminUser);
-        $manager->flush();
-
-
-        $manager->persist($movie);
-        function createSeatsForTheater($manager,MovieTheater $theater, $rows, $seatsPerRow, $seatType) {
-            foreach (range('A', chr(ord('A') + $rows - 1)) as $row) {
-                for ($number = 1; $number <= $seatsPerRow; $number++) {
-                    $seat = new Seat();
-                    $seat->setSeatRow($row);
-                    $seat->setSeatNumber((string)$number);
-                    $seat->setSeatType($seatType); // Assuming seat type is uniform for simplicity
-                    $theater->addSeat($seat);
-                    $manager->persist($seat);
-                }
-            }
-        }
-
-        // Creating multiple cinemas and theaters
-        $cinemas = [];
-        for ($i = 1; $i <= 3; $i++) {
-            $cinema = new Cinema();
-            $cinema->setName("Cinema $i");
-            $cinema->setCity("City $i");
-            $cinema->setAddress("Address $i");
-            $cinema->setCoords('Coords $i');
-
-            $movieTheater = new MovieTheater();
-            $movieTheater->setName("Theater $i");
-
-            // Create seats for each theater
-            createSeatsForTheater($manager, $movieTheater, 10, 15, 'Standard'); // Example: 10 rows, 15 seats per row
-
-            $cinema->addMovieTheater($movieTheater);
-            $manager->persist($cinema);
-            $manager->persist($movieTheater);
-
-            $cinemas[] = $cinema;
-        }
-
-        // Persists and flushes for each cinema
-        foreach ($cinemas as $cinema) {
-            $manager->persist($cinema);
-            foreach ($cinema->getMovieTheaters() as $theater) {
-                foreach ($theater->getSeats() as $seat) {
-                    $manager->persist($seat);
-                }
-            }
-        }
-
-        $cinema1 = $cinemas[0]; // Assuming $cinemas is the array holding the cinema entities
-        $theater1 = $cinema1->getMovieTheaters()[0]; // Assuming getMovieTheaters() returns an array or a Collection
-
-        // Create additional movies
-        $movies = [
-            ['title' => 'The Matrix', 'description' => 'Sci-fi classic', 'director' => 'The Wachowskis', 'genre' => 'Sci-Fi', 'image' => 'images/matrix.jpg', 'rating' => 8.7],
-            ['title' => 'Inception', 'description' => 'Dream within a dream', 'director' => 'Christopher Nolan', 'genre' => 'Action', 'image' => 'images/inception.jpg', 'rating' => 8.8],
-            // ... add as many as you like
-        ];
-
-        foreach ($movies as $movieData) {
-            $movie = new Movie();
-            $movie->setTitle($movieData['title']);
-            $movie->setDescription($movieData['description']);
-            $movie->setDirector($movieData['director']);
-            $movie->setGenre($movieData['genre']);
-            $movie->setDuration(new DateTimeImmutable());
-            $movie->setReleaseYear(new DateTime());
-            $movie->setImage($movieData['image']);
-            $movie->setRating($movieData['rating']);
-
-            $manager->persist($movie);
-
-            // Create a screening for each movie
-            $screening = new Screening();
-            $screening->setMovie($movie);
-            $screening->setStartTime(new DateTimeImmutable('next Saturday 8pm')); // Example start time, adjust as needed
-
-            // Associate the screening with Theater 1 in Cinema 1
-            $theater1->addScreening($screening);
-            $manager->persist($screening);
-        }
-
-        // Persisting the updated theater and cinema
-        $manager->persist($theater1);
         $manager->persist($cinema1);
-
-        // Flush to save changes to the database
+        $manager->persist($cinema2);
+        $manager->persist($cinema3);
         $manager->flush();
-
-
-
-
 
 
     }
