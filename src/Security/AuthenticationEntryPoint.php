a175d6c65d733581cb17a1b681163409b23d6a54
Besp1N <kackar87@wp.pl>
2024-02-05 14:48:24 +0000

    working app

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -22,7 +22,7 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
         if ($authException instanceof InactiveUserException) {
             $request->getSession()->getFlashBag()->add('note', 'Your account is not active. Please confirm your email.');
         } else {
-            $request->getSession()->getFlashBag()->add('note', 'You have to login in order to access this page or confirm email.');
+            $request->getSession()->getFlashBag()->add('note', 'You have to login in order to access this page.');
         }
 
         return new RedirectResponse($this->urlGenerator->generate('app_login'));
